f64e74ffd731e6a5551a948d0cc68e85c1965387
lukas <lukas.cristofaro@ynov.com>
2024-05-14 23:08:33 +0200

    update home page style

--- index.php
+++ index.php
@@ -11,43 +11,43 @@
 
 <body>
 
-<div class="soustitre">
-      <div class="title">
-        <img class="petiteetoilefonce" src="<?php echo get_template_directory_uri(); ?>/asset/img/petite_etoile_foncé.svg"/>
-        <img class="cercle" src="<?php echo get_template_directory_uri(); ?>/asset/img/cercle.svg"/>
-        <h2 class="subtitle" id="equipe">        
-            <?php
+    <div class="soustitre">
+        <div class="title">
+            <img class="petiteetoilefonce" src="<?php echo get_template_directory_uri(); ?>/asset/img/petite_etoile_foncé.svg" />
+            <img class="cercle" src="<?php echo get_template_directory_uri(); ?>/asset/img/cercle.svg" />
+            <h2 class="subtitle" id="equipe">
+                <?php
                 $content = get_the_content();
                 require_once(ABSPATH . 'wp-content/themes/livlyon/getBlock.php');
                 getBlock($content, 2);
+                ?>
+            </h2>
+        </div>
+        <p class="paragrapheequipe">
+            <?php
+            $content = get_the_content();
+            require_once(ABSPATH . 'wp-content/themes/livlyon/getBlock.php');
+            getBlock($content, 4);
             ?>
-        </h2>
-      </div>  
-      <p class="paragrapheequipe">
-      <?php
-                $content = get_the_content();
-                require_once(ABSPATH . 'wp-content/themes/livlyon/getBlock.php');
-                getBlock($content, 4);
-            ?>
-      </p>
+        </p>
     </div>
 
 
     <div class="cards">
-<?php 
-$args = array('post_type' => 'post');
-$my_query = new WP_Query($args);
+        <?php
+        $args = array('post_type' => 'post');
+        $my_query = new WP_Query($args);
 
-if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); ?>
-    <?php echo '<div class="card">' ?>
-    <?php echo the_post_thumbnail(); ?>
-    <?php echo '</div>'; ?>
-<?php endwhile; ?>
-<?php else: ?>
-    <p>Il n'y a pas de contenu à afficher</p>
-<?php endif; ?>
-</div>
-   
+        if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); ?>
+                <?php echo '<div class="card">' ?>
+                <?php echo the_post_thumbnail(); ?>
+                <?php echo '</div>'; ?>
+            <?php endwhile; ?>
+        <?php else : ?>
+            <p>Il n'y a pas de contenu à afficher</p>
+        <?php endif; ?>
+    </div>
+
     <div>
         <?php
 
@@ -66,4 +66,5 @@ if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_
         }
     });
 </script>
+
 </html>
