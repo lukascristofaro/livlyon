1cd86d92e8fca56f6dde5f57633c2316c3734e1b
lukas <lukas.cristofaro@ynov.com>
2024-05-14 23:23:41 +0200

    update home page style

--- header.php
+++ header.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/asset/css/header.css">
     <title>Liv'Lyon</title>
+    <!-- ajout d'une icone pour le site -->
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/asset/img/livlyon.png" />
 </head>
 
 <body>
